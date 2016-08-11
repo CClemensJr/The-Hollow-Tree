7a5b9e4b5f7c03e1125a3acaacf4585be099399c
Charles Clemens <cclemensjr@gmail.com>
2016-08-10 23:09:50 -0700

    Added age code

--- theHollowTree.js
+++ theHollowTree.js
@@ -5,3 +5,13 @@ confirm("Shall we begin?");
 
 // Determine player age for some reason
 var age = prompt("What is your age?");
+
+if age <  13
+{
+	console.log("A fine age to play!");
+}
+else
+{
+	console.log("I'll let you play but you really should go outside or read a book or something other than being glued to your computer.");
+}
+
