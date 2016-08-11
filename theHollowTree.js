21c9afc3d88861b96b4ad5c385fab097893b8e0f
Charles Clemens <cclemensjr@gmail.com>
2016-08-10 23:18:27 -0700

    Added initial scene for story

--- theHollowTree.js
+++ theHollowTree.js
@@ -15,3 +15,6 @@ else
 	console.log("I'll let you play but you really should go outside or read a book or something other than being glued to your computer.");
 }
 
+//Set the initial scene for the story.
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
