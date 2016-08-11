a230636d2c9866e60483d9bc01513a71c7e975fc
Charles Clemens <cclemensjr@gmail.com>
2016-08-10 23:39:22 -0700

    Added bieber prompt 1

--- theHollowTree.js
+++ theHollowTree.js
@@ -18,3 +18,5 @@ else
 //Set the initial scene for the story.
 console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
 console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt("Do you want to race Bieber on stage?");
